2089db74ef1ff74da448f1509b69b5bfd199e19d
Joshua Lambert <jlambert@abatgroup.de>
2023-03-13 09:41:57 +0100

    Add new model

---
+++ app/model/song.py
@@ -0,0 +1,11 @@
+from .. import db
+
+class Song(db.Model):
+    """Song Model for storing song related details"""
+
+    __tablename__ = "song"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    interpret = db.Column(db.String(250))
+    title = db.Column(db.String(250))
+    length = db.Column(db.Integer())
