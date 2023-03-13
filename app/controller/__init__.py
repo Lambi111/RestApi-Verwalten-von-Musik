627bd82ed715033a52e09600b27056bcf15bf543
Joshua Lambert <jlambert@abatgroup.de>
2023-03-13 13:41:23 +0100

    Updated Requirements

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,3 +1,4 @@
 from .user import api as user_namespace
+from .song import api as song_namespace
 from .auth import api as auth_api
 from .role import api as role_api
--- app/model/__init__.py
+++ app/model/__init__.py
@@ -1,4 +1,5 @@
 from app.model.user import *
 from app.model.role import *
+from app.model.song import *
 from app.model.user_role import *
 from app.model.blacklist_token import *
